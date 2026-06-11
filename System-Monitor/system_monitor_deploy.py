af738b70125c884e97a47d2d3f93f557f248bfc9
semrapasa <semra.a.pasa@gmail.com>
2026-06-11 08:43:17 +0300

    fix: errors on deploy
    Deploy edilirken ve sonrsındaa hata veren kodlar düzeltildi

--- System-Monitor/system_monitor_deploy.py
+++ System-Monitor/system_monitor_deploy.py
@@ -2,24 +2,24 @@ import psutil
 import asyncio
 from fastapi import FastAPI, BackgroundTasks
 
-sistem_app = FastAPI(title="SİSTEM MONİTÖRÜ API")
+sistem_app = FastAPI(title="SISTEM MONITORU API")
 izleme_aktif = False
 canli_veri = {
-    "cpu": "Ölçüm başlatın",
-    "ram": "Ölçüm başlatın"
+    "cpu": "Olcum baslatin",
+    "ram": "Olcum baslatin"
 }
 
 
-async def system_monitor(bars=30):
+async def system_monitor(bars=50):
     global izleme_aktif, canli_veri
     while izleme_aktif:
-        cpu_yuzde = psutil.cpu_percent()
+        cpu_yuzde = psutil.cpu_percent(interval=0.1)
         ram_yuzde = psutil.virtual_memory().percent
         cpu_oran = cpu_yuzde / 100.0
-        cpu_bar = "█" * int(cpu_oran * bars) + "-" * \
+        cpu_bar = "█" * int(cpu_oran * bars) + "░" * \
             (bars - int(cpu_oran * bars))
         ram_oran = ram_yuzde / 100.0
-        ram_bar = "█" * int(ram_oran * bars) + "-" * \
+        ram_bar = "█" * int(ram_oran * bars) + "░" * \
             (bars - int(ram_oran * bars))
         canli_veri = {
             "cpu": f"|{cpu_bar}| %{cpu_yuzde:.2f}",
@@ -31,7 +31,7 @@ async def system_monitor(bars=30):
 @sistem_app.get("/")
 def ana_menu():
     return {
-        "mesaj": "Sistem izleme monitörüne hoş geldiniz. Programı çalıştırmak için url sonuna /docs ekleyiniz",
+        "mesaj": "Programi calistirmak icin sistem_monitor_baslat ardından ana_menu kısmını tekrar tekrar execute edebilirsiniz",
         "sistem_durumu": canli_veri}
 
 
@@ -42,11 +42,11 @@ def system_monitor_stop():
     return {"mesaj": "Durduruldu"}
 
 
-@sistem_app.get("/sistem_monitör_başlat")
+@sistem_app.get("/sistem_monitor_baslat")
 async def system_monitor_start(background_tasks: BackgroundTasks):
     global izleme_aktif
     if not izleme_aktif:
         izleme_aktif = True
 
     background_tasks.add_task(system_monitor)
-    return {"mesaj": "Başlatıldı"}
+    return {"mesaj": "Baslatildi"}
